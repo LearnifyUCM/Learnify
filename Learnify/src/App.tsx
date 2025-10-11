c22e0f31c73076ecc5415ec1015b7a9677de460b
Angel Vazquez <vazquez73195@gmail.com>
2025-10-11 00:16:06 -0700

    Initial Commit

---
+++ Learnify/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import FileUpload from "./FileUpload.tsx";
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <FileUpload />
+      </div>
+    </>
+  )
+}
+
+export default App
